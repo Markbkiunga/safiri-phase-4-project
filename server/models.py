d5096c133ee93953eec81ffded1582f1cdef9f4b
Mark Brian <markbkiunga2017@gmail.com>
2024-10-18 21:03:42 +0300

    Gone through the code fixing debugging and testing

--- server/models.py
+++ server/models.py
@@ -1,33 +1,42 @@
 from datetime import datetime
+import pytz
+from sqlalchemy import DateTime
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-from sqlalchemy.orm import validates, relationship
-from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
-
-db = SQLAlchemy()
+from config import db
+gmt_plus_3 = pytz.timezone("Africa/Nairobi")
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
-    
+    serialize_rules = ('-user_activities.user','-activities.users', '-reviews.user', '-profile.user') 
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
+    created_at = db.Column( 
+            DateTime(timezone=True),
+            server_default=db.func.now(),
+            default=datetime.now(gmt_plus_3),)
+    updated_at = db.Column(
+        DateTime(timezone=True),
+        onupdate=datetime.now(gmt_plus_3),
+        default=datetime.now(gmt_plus_3),)
 
-    activities = db.relationship('UserActivity', back_populates='user')
-    activity_names = association_proxy('activities', 'activity_name')  
+    user_activities = db.relationship('UserActivity', back_populates='user', cascade="all, delete-orphan")
+    activities = association_proxy('user_activities', 'activity', creator=lambda activity_obj: UserActivity(activity = activity_obj))
+    sites = association_proxy('reviews', 'site', creator=lambda site_obj: Review(site = site_obj))
 
-    reviews = db.relationship('Review', back_populates='user')
+    reviews = db.relationship('Review', back_populates='user', cascade="all, delete-orphan")
     profile = db.relationship('Profile', uselist=False, back_populates='user', cascade='all, delete-orphan')
 
     @validates('username')
-    def validate_username(self, key, value):
-        if len(value) < 5:
+    def validate_username(self, key, username):
+        if len(username) < 5:
             raise ValueError('Username must be at least 5 characters long')
-        return value
+        return username
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -35,20 +44,11 @@ class User(db.Model, SerializerMixin):
     def check_password(self, password):
         return check_password_hash(self.password, password)
     
-    serialize_rules = ('-password',) 
     
-    def serialize(self):
-        return {
-            'id': self.id,
-            'username': self.username,
-            'created_at': self.created_at.strftime('%m/%d/%Y'),  
-            'profile': self.profile.serialize() if self.profile else None,
-            'activities': self.activity_names 
-        }
-
 
 class Profile(db.Model, SerializerMixin):
     __tablename__ = 'profiles'
+    serialize_rules = ('-user.profile',)
     
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50), nullable=False)
@@ -57,56 +57,55 @@ class Profile(db.Model, SerializerMixin):
     image = db.Column(db.String(255))
     bio = db.Column(db.Text)
     phone_number = db.Column(db.String(20))
+
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     user = db.relationship('User', back_populates='profile')
 
     @validates('email')
-    def validate_email(self, key, value):
-        if '@' not in value:
-            raise ValueError('Invalid email format')
-        return value
-
-    def serialize(self):
-        return {
-            'id': self.id,
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'email': self.email,
-            'image': self.image,
-            'bio': self.bio,
-            'phone_number': self.phone_number
-        }
+    def validate_email(self, key, email):
+        if '@' not in email:
+            raise ValueError('Email should contain @')
+        return email
 
 
 class UserActivity(db.Model, SerializerMixin):
     __tablename__ = 'user_activities'
+    serialize_rules = ('-user.user_activities', '-activity.user_activities',)
     
     id = db.Column(db.Integer, primary_key=True)
     feedback = db.Column(db.Text)
-    participation_date = db.Column(db.DateTime, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
+    participation_date = db.Column(DateTime(timezone=True), nullable=False)
+    created_at = db.Column(
+            DateTime(timezone=True),
+            server_default=db.func.now(),
+            default=datetime.now(gmt_plus_3),)
+    updated_at = db.Column(
+        DateTime(timezone=True),
+        onupdate=datetime.now(gmt_plus_3),
+        default=datetime.now(gmt_plus_3),)
+    
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     activity_id = db.Column(db.Integer, db.ForeignKey('activities.id'), nullable=False)
 
-    user = db.relationship('User', back_populates='activities')
-    activity = db.relationship('Activity', back_populates='participants')
-
-    # This one will be used in association_proxy
-    @property
-    def activity_name(self):
-        return self.activity.name if self.activity else None  
-
+    user = db.relationship('User', back_populates='user_activities')
+    activity = db.relationship('Activity', back_populates='user_activities')
 
 class Review(db.Model, SerializerMixin):
     __tablename__ = 'reviews'
-    
+    serialize_rules = ('-user.reviews', '-site.reviews',)
+
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.Text, nullable=False)
     rating = db.Column(db.Integer, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
+    created_at = db.Column(
+            DateTime(timezone=True),
+            server_default=db.func.now(),
+            default=datetime.now(gmt_plus_3),)
+    updated_at = db.Column(
+        DateTime(timezone=True),
+        onupdate=datetime.now(gmt_plus_3),
+        default=datetime.now(gmt_plus_3),)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     site_id = db.Column(db.Integer, db.ForeignKey('sites.id'), nullable=False)
 
@@ -115,51 +114,47 @@ class Review(db.Model, SerializerMixin):
 
     @validates('rating')
     def validate_rating(self, key, value):
-        if not (1 <= value <= 5):
-            raise ValueError('Rating must be between 1 and 5')
+        if not (1 <= value <= 10):
+            raise ValueError('Rating must be between 1 and 10')
         return value
-
-    def serialize(self):
-        return {
-            'id': self.id,
-            'description': self.description,
-            'rating': self.rating,
-            'created_at': self.created_at.strftime('%m/%d/%Y'),
-            'user_id': self.user_id
-        }
-
 
 class Activity(db.Model, SerializerMixin):
     __tablename__ = 'activities'
+    serialize_rules = ('-user_activities.activity','-users.activities', '-site_activities.activity',)
     
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.Text)
     name = db.Column(db.String(100), nullable=False)
     category = db.Column(db.String(50))
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
+    created_at = db.Column( 
+            DateTime(timezone=True),
+            server_default=db.func.now(),
+            default=datetime.now(gmt_plus_3),)
+    updated_at = db.Column(
+        DateTime(timezone=True),
+        onupdate=datetime.now(gmt_plus_3),
+        default=datetime.now(gmt_plus_3),)
     
-    participants = db.relationship('UserActivity', back_populates='activity')
-    site_activities = db.relationship('SiteActivity', back_populates='activity', cascade='all, delete-orphan')
+    user_activities = db.relationship('UserActivity', back_populates='activity', cascade='all, delete-orphan')
+    users = association_proxy("user_activities", "user", creator=lambda user_obj: UserActivity(user = user_obj))
 
-    def serialize(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'description': self.description,
-            'category': self.category,
-            'created_at': self.created_at.strftime('%m/%d/%Y'), 
-            # Included the  participant usernames
-            'participants': [user_activity.user.username for user_activity in self.participants]  
-        }
+    site_activities = db.relationship('SiteActivity', back_populates='activity', cascade='all, delete-orphan')
 
 
 class SiteActivity(db.Model):
     __tablename__ = 'site_activities'
+    serialize_rules = ('-site.site_activities','-activity.site_activities',)
     
     id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
+    created_at = db.Column( 
+            DateTime(timezone=True),
+            server_default=db.func.now(),
+            default=datetime.now(gmt_plus_3),)
+    updated_at = db.Column(
+        DateTime(timezone=True),
+        onupdate=datetime.now(gmt_plus_3),
+        default=datetime.now(gmt_plus_3),)
+    
     activity_id = db.Column(db.Integer, db.ForeignKey('activities.id'), nullable=False)
     site_id = db.Column(db.Integer, db.ForeignKey('sites.id'), nullable=False)
 
@@ -169,6 +164,7 @@ class SiteActivity(db.Model):
 
 class Site(db.Model, SerializerMixin):
     __tablename__ = 'sites'
+    serialize_rules = ('-site_activities.site','-reviews.site', '-location.sites', '-users.sites',)
     
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
@@ -176,38 +172,31 @@ class Site(db.Model, SerializerMixin):
     description = db.Column(db.Text)
     is_saved = db.Column(db.Boolean, default=False)
     category = db.Column(db.String(50))
+
     location_id = db.Column(db.Integer, db.ForeignKey('locations.id'), nullable=False)
     
-    activities = db.relationship('Activity', secondary='site_activities', back_populates='site')
-    reviews = db.relationship('Review', back_populates='site')
+    site_activities = db.relationship('SiteActivity', back_populates='site', cascade='all, delete-orphan')
+    reviews = db.relationship('Review', back_populates='site', cascade='all, delete-orphan')
     location = db.relationship('Location', back_populates='sites')
 
-    def serialize(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'description': self.description,
-            'category': self.category,
-            'is_saved': self.is_saved,
-            'activities': [activity.serialize() for activity in self.activities]
-        }
+    users = association_proxy('reviews', 'user', creator=lambda user_obj: Review(user= user_obj))
 
 
 class Location(db.Model, SerializerMixin):
     __tablename__ = 'locations'
+    serialize_rules = ('-sites.location',)
     
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     image = db.Column(db.String(255))
     description = db.Column(db.Text)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
+    created_at = db.Column(
+            DateTime(timezone=True),
+            server_default=db.func.now(),
+            default=datetime.now(gmt_plus_3),)
+    updated_at = db.Column(
+        DateTime(timezone=True),
+        onupdate=datetime.now(gmt_plus_3),
+        default=datetime.now(gmt_plus_3),)
 
     sites = db.relationship('Site', back_populates='location', cascade='all, delete-orphan')
-
-    def serialize(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'description': self.description
-        }
