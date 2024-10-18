2b78e2b5905277fb4926bbe59b5a10993790ff58
Anthony <anthony.onyango@student.moringaschool.com>
2024-10-18 12:44:54 +0300

    implemented the profile model with one to one relationship to the user

--- server/models.py
+++ server/models.py
@@ -21,3 +21,17 @@ class User(db.Model):
     
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+class Profile(db.Model):
+    __tablename__ = 'profiles'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    image = db.Column(db.String(255))
+    bio = db.Column(db.Text)
+    phone_number = db.Column(db.String(20))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    user = db.relationship('User', back_populates='profile')
