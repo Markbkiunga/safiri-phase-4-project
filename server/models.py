9fcfd4eb436f2a0f25206c722716c700ff9cfa3e
Anthony <anthony.onyango@student.moringaschool.com>
2024-10-18 12:40:06 +0300

    implemented the user models with password hashing

--- server/models.py
+++ server/models.py
@@ -1,8 +1,20 @@
-from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.ext.associationproxy import association_proxy
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
-from config import db
+db = SQLAlchemy()
 
-# Models go here!
-class User():
-    pass
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
