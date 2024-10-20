c6ac3525071238591ae9ff05b96999ef3dd647b3
Mark Brian <markbkiunga2017@gmail.com>
2024-10-20 14:59:56 +0300

    Updates Review component CSS and JSX, added Review API endpoints for CRUD operations, and modified server-side code to handle reviews

--- client/src/components/Review/Review.jsx
+++ client/src/components/Review/Review.jsx
@@ -1,169 +1,196 @@
-import React, { useState, useEffect } from 'react';  
-import NavBar from '../NavBar/NavBar';  
-import './Review.css';  
-import logo from '../pictures/SAFIRI LOGO.png';  
-import Footer from '../Footer/Footer';  
-import { Formik, Form, Field, ErrorMessage } from 'formik';  
-import * as Yup from 'yup';  
+import React, { useState, useEffect } from 'react';
+import NavBar from '../NavBar/NavBar';
+import './Review.css';
+import logo from '../pictures/SAFIRI LOGO.png';
+import Footer from '../Footer/Footer';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
-const Review = () => {  
-  const [reviews, setReviews] = useState([]);  
+const Review = () => {
+  const [reviews, setReviews] = useState([]);
 
-  useEffect(() => {  
-    const fetchReviews = async () => {  
-      try {  
-        const response = await fetch('/reviews');  
-        if (!response.ok) {  
-          throw new Error('Network response was not ok');  
-        }  
-        const data = await response.json();  
-        if (Array.isArray(data)) {  
-          setReviews(data);  
-        } else {  
-          console.error('Unexpected data structure:', data);  
-          setReviews([]);  
-        }  
-      } catch (error) {  
-        console.error('Error fetching reviews:', error);  
-        setReviews([]);  
-      }  
-    };  
+  useEffect(() => {
+    const fetchReviews = async () => {
+      try {
+        const response = await fetch('/reviews');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        if (Array.isArray(data)) {
+          setReviews(data);
+          console.log(data);
+        } else {
+          console.error('Unexpected data structure:', data);
+          setReviews([]);
+        }
+      } catch (error) {
+        console.error('Error fetching reviews:', error);
+        setReviews([]);
+      }
+    };
 
-    fetchReviews();  
-  }, []);  
+    fetchReviews();
+  }, []);
 
-  const validationSchema = Yup.object().shape({  
-    name: Yup.string().required('Required'),  
-    place: Yup.string().required('Required'),  
-    reviewText: Yup.string().required('Required'),  
-    source: Yup.string().required('Required'),  
-  });  
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required('Required'),
+    place: Yup.string().required('Required'),
+    reviewText: Yup.string().required('Required'),
+    source: Yup.string().required('Required'),
+  });
 
-  const handleSubmit = (values, { resetForm }) => {  
-    const newReview = {  
-      id: Date.now(),  
-      name: values.name,  
-      place: values.place,  
-      review: values.reviewText,  
-      image: values.image,  
-      rating: values.rating,  
-      source: values.source,  
-    };  
+  const handleSubmit = (values, { resetForm }) => {
+    const newReview = {
+      id: Date.now(),
+      name: values.name,
+      place: values.place,
+      review: values.reviewText,
+      image: values.image,
+      rating: values.rating,
+      source: values.source,
+    };
 
-    console.log(newReview);  
-    setReviews([...reviews, newReview]);  
-    resetForm();  
-  };  
+    console.log(newReview);
+    setReviews([...reviews, newReview]);
+    resetForm();
+  };
 
-  return (  
-    <div id='review-page'>  
-      <NavBar />  
-      <img src={logo} alt="safiri-logo" id="safiri-logo" />  
-      <h1>Review</h1>  
+  return (
+    <div id="review-page">
+      <NavBar />
+      <img src={logo} alt="safiri-logo" id="safiri-logo" />
+      <h1>Review</h1>
 
-      <div className="review-container">  
-        <div className="form-container">  
-          <h2>Submit Your Review</h2>  
-          <Formik  
-            initialValues={{  
-              name: '',  
-              place: '',  
-              reviewText: '',  
-              image: '',  
-              rating: null,  
-              source: '',  
-            }}  
-            validationSchema={validationSchema}  
-            onSubmit={handleSubmit}  
-          >  
-            {({ values, setFieldValue }) => (  
-              <Form>  
-                <div className="form-group">  
-                  <label htmlFor="name">Name:</label>  
-                  <Field type="text" id="name" name="name" />  
-                  <ErrorMessage name="name" component="div" className="error" />  
-                </div>  
-                <div className="form-group">  
-                  <label htmlFor="place">Place Visited:</label>  
-                  <Field type="text" id="place" name="place" />  
-                  <ErrorMessage name="place" component="div" className="error" />  
-                </div>  
-                <div className="form-group">  
-                  <label htmlFor="reviewText">Review:</label>  
-                  <Field as="textarea" id="reviewText" name="reviewText" />  
-                  <ErrorMessage name="reviewText" component="div" className="error" />  
-                </div>  
-                <div className="form-group">  
-                  <label htmlFor="image">Image:</label>  
-                  <Field type="text" id="image" name="image" placeholder="Enter image URL (optional)" />  
-                </div>  
-                <div className="form-group">  
-                  <label>How was your experience using our website?</label>  
-                  <div className="rating">  
-                    {Array.from({ length: 10 }, (_, i) => (  
-                      <span  
-                        key={i + 1}  
-                        className={`circle ${values.rating === i + 1 ? 'selected' : ''}`}  
-                        onClick={() => setFieldValue('rating', i + 1)}  
-                      >  
-                        {i + 1}  
-                      </span>  
-                    ))}  
-                  </div>  
-                </div>  
-                <div className="form-group">  
-                  <label htmlFor="source">How did you hear about us?</label>  
-                  <Field as="select" id="source" name="source">  
-                    <option value="" label="Select an option" />  
-                    <option value="friends">From Friends</option>  
-                    <option value="family">From Family</option>  
-                    <option value="advertisement">From an Advertisement</option>  
-                    <option value="social-media">From Social Media</option>  
-                  </Field>  
-                  <ErrorMessage name="source" component="div" className="error" />  
-                </div>  
-                <button type="submit" id='submit-review-button'>Submit Review</button>  
-              </Form>  
-            )}  
-          </Formik>  
-        </div>  
-        <div className="existing-reviews new-reviews">  
-          <h2>Reviews from Other Tourists</h2>  
-          {reviews.length > 0 ? (  
-            <ul>  
-              {reviews.map((review) => (  
-                <li key={review.id} className="review-item">  
-                  {review.image && (  
-                    <img  
-                      src={review.image}  
-                      alt={`${review.name}'s review`}  
-                      className="review-picture"  
-                    />  
-                  )}  
-                  <h3>{review.name}</h3>  
-                  {review.place && (  
-                    <p>  
-                      <strong>Place Visited:</strong> {review.place}  
-                    </p>  
-                  )}  
-                  {review.review && (  
-                    <p>  
-                      <strong>Review:</strong> {review.review}  
-                    </p>  
-                  )}  
-                </li>  
-              ))}  
-            </ul>  
-          ) : (  
-            <p>Loading...</p>  
-          )}  
-        </div>  
-      </div>  
-      <div id='review-page-footer'>  
-        <Footer />  
-      </div>  
-    </div>  
-  );  
-};  
+      <div className="review-container">
+        <div className="form-container">
+          <h2>Submit Your Review</h2>
+          <Formik
+            initialValues={{
+              name: '',
+              place: '',
+              reviewText: '',
+              image: '',
+              rating: null,
+              source: '',
+            }}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ values, setFieldValue }) => (
+              <Form>
+                <div className="form-group">
+                  <label htmlFor="name">Name:</label>
+                  <Field type="text" id="name" name="name" />
+                  <ErrorMessage name="name" component="div" className="error" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="place">Place Visited:</label>
+                  <Field type="text" id="place" name="place" />
+                  <ErrorMessage
+                    name="place"
+                    component="div"
+                    className="error"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="reviewText">Review:</label>
+                  <Field as="textarea" id="reviewText" name="reviewText" />
+                  <ErrorMessage
+                    name="reviewText"
+                    component="div"
+                    className="error"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="image">Image:</label>
+                  <Field
+                    type="text"
+                    id="image"
+                    name="image"
+                    placeholder="Enter image URL (optional)"
+                  />
+                </div>
+                <div className="form-group">
+                  <label>How was your experience using our website?</label>
+                  <div className="rating">
+                    {Array.from({ length: 10 }, (_, i) => (
+                      <span
+                        key={i + 1}
+                        className={`circle ${
+                          values.rating === i + 1 ? 'selected' : ''
+                        }`}
+                        onClick={() => setFieldValue('rating', i + 1)}
+                      >
+                        {i + 1}
+                      </span>
+                    ))}
+                  </div>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="source">How did you hear about us?</label>
+                  <Field as="select" id="source" name="source">
+                    <option value="" label="Select an option" />
+                    <option value="friends">From Friends</option>
+                    <option value="family">From Family</option>
+                    <option value="advertisement">From an Advertisement</option>
+                    <option value="social-media">From Social Media</option>
+                  </Field>
+                  <ErrorMessage
+                    name="source"
+                    component="div"
+                    className="error"
+                  />
+                </div>
+                <button type="submit" id="submit-review-button">
+                  Submit Review
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </div>
+        <div className="existing-reviews new-reviews">
+          <h2>Reviews from Other Tourists</h2>
+          {reviews.length > 0 ? (
+            <ul>
+              {reviews.map((review) => (
+                <li key={review.id} className="review-item">
+                  {review.user.profile.image && (
+                    <img
+                      src={review.user.profile.image}
+                      alt={`${review.user.username}'s review`}
+                      className="review-picture"
+                    />
+                  )}
+                  <h3>{review.user.username}</h3>
+                  {review.site.description && (
+                    <p>
+                      <strong>Place Visited:</strong> {review.site.description}
+                    </p>
+                  )}
+                  {review.description && (
+                    <p>
+                      <strong>Review:</strong> {review.description}
+                    </p>
+                  )}
+                  {review.rating && (
+                    <p>
+                      <strong>Rating:</strong> {review.rating}
+                    </p>
+                  )}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+      </div>
+      <div id="review-page-footer">
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
 export default Review;
--- server/app.py
+++ server/app.py
@@ -3,17 +3,30 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request, session
-from flask_restful import Resource
+
+from flask import request, session, Blueprint, make_response, jsonify
+from flask_restful import Resource, Api, reqparse
 
 # Local imports
 from config import app, db, api
 
 # Add your model imports
-from models import User, Profile, UserActivity, Review, SiteActivity, Site, Location
+from models import User, Profile, UserActivity, Review, SiteActivity, Site, Location, Activity
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from datetime import datetime
+import pytz
 
 
-# Views go here!
+from sqlalchemy.exc import IntegrityError
+
+gmt_plus_3 = pytz.timezone("Africa/Nairobi")
+
+#initializing Flask-Restful Api
+
+
+
 
 
 @app.route("/")
@@ -70,7 +83,79 @@ class Signup(Resource):
             print(e)
             return {"error": f"{str(e)}"}, 500
 
+class ReviewList(Resource):
+    def get(self):
+        # Get all reviews
+        reviews = Review.query.all()
+        return jsonify([review.to_dict() for review in reviews])
 
+    def post(self):
+        # Parse input data
+        parser = reqparse.RequestParser()
+        parser.add_argument('description', required=True, help="Description is required")
+        parser.add_argument('rating', type=int, required=True, help="Rating is required")
+        parser.add_argument('user_id', type=int, required=True)
+        parser.add_argument('site_id', type=int, required=True)
+        data = parser.parse_args()
+
+        # Check if user and site exist
+        user = User.query.get(data['user_id'])
+        site = Site.query.get(data['site_id'])
+
+        if not user or not site:
+            return jsonify({"error": "User or Site not found"}), 404
+
+        # Create a new review
+        new_review = Review(
+            description=data['description'],
+            rating=data['rating'],
+            user_id=user.id,
+            site_id=site.id,
+            created_at=datetime.now(gmt_plus_3)
+        )
+
+        # Save the review in the database
+        db.session.add(new_review)
+        db.session.commit()
+        return jsonify(new_review.to_dict()), 201
+
+# Class to handle individual review actions
+class ReviewDetail(Resource):
+    def get(self, id):
+        review = Review.query.get(id)
+        if not review:
+            return jsonify({"error": "Review not found"}), 404
+        return jsonify(review.to_dict())
+
+    def patch(self, id):
+        review = Review.query.get(id)
+        if not review:
+            return jsonify({"error": "Review not found"}), 404
+
+        # Parse input data to update review
+        parser = reqparse.RequestParser()
+        parser.add_argument('description', type=str)
+        parser.add_argument('rating', type=int)
+        data = parser.parse_args()
+
+        review.description = data.get('description', review.description)
+        review.rating = data.get('rating', review.rating)
+        review.updated_at = datetime.now(gmt_plus_3)
+
+        db.session.commit()
+        return jsonify(review.to_dict())
+
+    def delete(self, id):
+        review = Review.query.get(id)
+        if not review:
+            return jsonify({"error": "Review not found"}), 404
+
+        db.session.delete(review)
+        db.session.commit()
+        return jsonify({"message": "Review deleted successfully"})
+
+api.add_resource(ReviewList, '/reviews', endpoint='reviews')
+api.add_resource(ReviewDetail, '/reviews/<int:id>', endpoint='review_detail')
 api.add_resource(Login, "/login", endpoint="login")
 api.add_resource(CheckSession, "/check_session", endpoint="check_session")
 api.add_resource(Logout, "/logout", endpoint="logout")
