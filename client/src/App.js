931cbc55274857297f7d00a63adda1057346fc59
Mark Brian <markbkiunga2017@gmail.com>
2024-10-23 16:07:28 +0300

    Updates App.js to include credentials and headers in fetch request, and updated config.py to specify CORS origins

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,13 @@ function App() {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    fetch('https://safiri-phase-4-project.onrender.com/check_session').then((response) => {
+    fetch('https://safiri-phase-4-project.onrender.com/check_session', {
+      method: 'GET',
+      credentials: 'include', // Ensure cookies are sent with the request
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((response) => {
       if (response.ok) {
         response.json().then((user) => setUser(user));
       }
