a606f8b59c65b761a6c318dc00ea6cb8c74446a8
Mark Brian <markbkiunga2017@gmail.com>
2024-10-24 10:22:38 +0300

    Refactors App.js and server code: simplify fetch call, remove print statements, and adjust CORS configuration

--- server/config.py
+++ server/config.py
@@ -16,19 +16,13 @@ from flask_cors import CORS
 
 # Instantiate app, set attributes
 app = Flask(__name__)
-CORS(
-    app,
-    origins=["https://safiri-phase-4-project.vercel.app"],
-    supports_credentials=True,
-)
+CORS(app)
 app.secret_key = os.getenv("SECRET_KEY")
 app.config["SECRET_KEY"] = os.environ.get(
     "SECRET_KEY", "b'\xeftXH\x10\xbf\xeb\xa7\xb5+\xc8S\xa4_\xe1\x0f'"
 )
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SESSION_COOKIE_SAMESITE"] = "None"
-app.config["SESSION_COOKIE_SECURE"] = True
 app.json.compact = False
 
 # Define metadata, instantiate db
