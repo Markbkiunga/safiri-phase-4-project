4f401ad97935eb9376b19ac1e07a72daba7ca881
Mark Brian <markbkiunga2017@gmail.com>
2024-10-24 08:53:12 +0300

    Adds session cookie security configurations to app.config

--- server/config.py
+++ server/config.py
@@ -21,6 +21,12 @@ app.secret_key = os.environ.get("SECRET_KEY")
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config.update(
+    SESSION_COOKIE_SECURE=True,  # Ensure it's set to True for HTTPS
+    SESSION_COOKIE_SAMESITE='Lax',  # Controls whether the session cookie is sent for cross-origin requests
+    SESSION_COOKIE_HTTPONLY=True   # Prevents client-side access to session cookies
+)
+
 app.json.compact = False
 
 # Define metadata, instantiate db
